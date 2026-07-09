103906539af203c6c782f6269821666c4d798710
sandarwin-pyae <sandarwpyae5@gmail.com>
2026-07-09 12:25:46 +0630

    fix : admin dashbard design (merging problem)

--- backend/api/main.py
+++ backend/api/main.py
@@ -37,7 +37,6 @@ app = FastAPI(title="Stationero Backend API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
-    allow_origin_regex=r"http://localhost:\d+|http://127\.0\.0\.1:\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
