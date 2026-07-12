9c8e53c2494315e1669f677d639e5efa8419d0bc
Theingi <theingiwinpyae03>
2026-07-12 22:38:19 +0630

    ProductDetail.jsx ,ProductPage.jsx and AuthContex.jsx are modified a little

--- Customer_Frontend/src/context/AuthContext.jsx
+++ Customer_Frontend/src/context/AuthContext.jsx
@@ -1,12 +1,16 @@
-// src/AuthContext.jsx
 import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    // 🌟 ဒီနေရာမှာ true ထားရင် အကုန်လုံး Login User Navbar ဖြစ်သွားမယ်
-    // false ထားရင် အကုန်လုံး Guest Navbar ဖြစ်သွားမယ်
-    const [isLoggedIn, setIsLoggedIn] = useState(false); 
+
+    const [isLoggedIn, setIsLoggedIn] = useState(() => {
+
+        const savedUser = localStorage.getItem("stationero_logged_user");
+
+        return savedUser ? true : false;
+
+    });
 
     return (
         <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
